c5335c99e893bf5e0893b89f47946bf289f77ea4
40756 <buturacristina18@stud.ase.ro>
2021-02-25 13:13:11 +0200

    Test project Java - Hello

---
+++ HelloWorld/src/ro/ase/csie/cts/laborator/TestHello.java
@@ -0,0 +1,11 @@
+package ro.ase.csie.cts.laborator;
+
+public class TestHello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello!"); // syso + ctrl + space
+		// !!! Alegem java project NU project !!!
+	}
+
+}
