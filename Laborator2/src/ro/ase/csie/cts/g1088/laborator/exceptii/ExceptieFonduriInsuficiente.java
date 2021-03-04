36b292d90cb39161bea702fde327d1afbbc6c372
40756 <buturacristina18@stud.ase.ro>
2021-03-04 12:25:06 +0200

    Laborator 2

---
+++ Laborator2/src/ro/ase/csie/cts/g1088/laborator/exceptii/ExceptieFonduriInsuficiente.java
@@ -0,0 +1,14 @@
+package ro.ase.csie.cts.g1088.laborator.exceptii;
+
+public class ExceptieFonduriInsuficiente extends Exception{
+	
+	// daca definim acest constructor cel default trebuie neaparat creat
+	public ExceptieFonduriInsuficiente(String mesaj) {
+		super(mesaj);
+	}
+	
+	public ExceptieFonduriInsuficiente() {
+		
+	}
+	
+}
